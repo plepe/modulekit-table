8cb5b0e5fa15a7b24a0ef5ad65659c217f28165f
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2015-02-27 09:30:56 +0100

    README: how to initialize table

---
+++ README.md
@@ -0,0 +1,18 @@
+```php
+$table = new table($definition, $data);
+print $table->show([$mode, [$param]])
+```
+
+```php
+$def = column definition, array:
+"column_id"=>array(
+  "name"=>
+  "format"=> (optional), define format of field using other values as 
+                         replacement patterns in [ ]
+                         e.g. "<a href='foo.html?id=[id]'>[name]</a>"
+
+$data = table data, array:
+"row_id"=>array(
+   "column_id"=>array("foobar")
+)
+```
--- inc/table.php
+++ inc/table.php
@@ -1,18 +1,4 @@
 <?
-/*
- *  $def = column definition, array:
- *  "column_id"=>array(
- *    "name"=>
- *    "format"=> (optional), define format of field using other values as 
- *                           replacement patterns in [ ]
- *                           e.g. "<a href='foo.html?id=[id]'>[name]</a>"
- *
- *  $data = table data, array:
- *  "row_id"=>array(
- *     "column_id"=>array("foobar")
- *  )
- */
-
 class table {
   function __construct($def, $data) {
     $this->def=$def;
